010a77f54a47055dd55d72e5af96d5007d2438a2
KoRIOz675 <thomas@mafille.net>
2025-05-24 12:18:39 +0200

    MEDIUM: ci: Updated contact form style

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -50,7 +50,7 @@ function Contact() {
               id="name"
               name="name"
               required
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"
+              className="mt-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"
             />
           </div>
           <div className="mb-4">
@@ -62,7 +62,7 @@ function Contact() {
               id="email"
               name="_replyto"
               required
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"
+              className="mt-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"
             />
           </div>
           <div className="mb-4">
@@ -74,7 +74,7 @@ function Contact() {
               name="message"
               rows={4}
               required
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"></textarea>
+              className="mt-1 block w-full rounded-md border-gray-300 px-4 py-2 shadow-sm focus:border-primary-500 focus:ring focus:ring-primary-500 focus:ring-opacity-50"></textarea>
           </div>
           <button
             type="submit"
